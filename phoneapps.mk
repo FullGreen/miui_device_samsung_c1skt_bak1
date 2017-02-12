c82deba1f1bf0049f8f53a5ba0c978d367ae2d24
fullgreen <abin030187@naver.com>
2017-02-13 00:01:06 +0900

    c1skt: Initial commit of makefile
    Signed-off-by: fullgreen <abin030187@naver.com>

---
+++ phoneapps.mk
@@ -0,0 +1,52 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	BasicDreams \
+	Bluetooth \
+	BluetoothExt \
+	CertInstaller \
+	Galaxy4 \
+	GoogleCalendarSyncAdapter \
+	GoogleContactsSyncAdapter \
+	GoogleTTS \
+	Initd \
+	KeyChain \
+	LiveWallpapers \
+	Nfc \
+	NoiseField \
+	PacProcessor \
+	PhaseBeam \
+	PhotoTable \
+	PicoTts \
+	PrintSpooler \
+	SamsungServiceMode \
+	Stk \
+	helper \
+	telresources \
+	WAPPushManager
+
+private-phone-priv-apps := \
+	BackupRestoreConfirmation \
+	SettingsProvider \
+	CellBroadcastReceiver \
+	ExternalStorageProvider \
+	FusedLocation \
+	GoogleBackupTransport \
+	GoogleFeedback \
+	GoogleLoginService \
+	GoogleOneTimeInitializer \
+	GooglePackageInstaller \
+	GooglePartnerSetup \
+	GoogleServicesFramework \
+	InputDevices \
+	LiveLockScreenService \
+	PrebuiltGmsCore \
+	ProxyHandler \
+	Screencast \
+	SharedStorageBackup \
+	Shell \
+	Snap \
+	Tag \
+	ThemesProvider \
+	TimeKeep
